418012785d18f6e250cb529b39ee0cddfe885c72
urooj <uroojismail48@gmail.com>
2026-08-11 05:12:46 +0500

    feat: add marquee/news ticker animation using GSAP

---
+++ scroll.js
@@ -0,0 +1,36 @@
+gsap.to(".page2 h1", {
+    transform :" translateX(-170%)",
+
+    scrollTrigger: {
+        trigger: ".page2",
+        scroller: "body",
+start: "top 0%" ,
+end: "top -100%",
+scrub: 2,
+pin : true       
+    },
+}) 
+     var tt =   gsap.timeline()
+tt.from(".navbar h3", {
+    opacity:0,
+    y:30,
+    delay: 0.8,
+    duration:1.2,
+    stagger : 1
+} )
+gsap.from(".main img", {
+      opacity:0,
+    y:30,
+    duration:4
+})
+gsap.from(".btns button", {
+      opacity:0,
+    y:30,
+    duration:4
+})
+gsap.from(".main h1", {
+      opacity:0,
+    y:30,
+    duration:4,
+    stagger:2
+})
